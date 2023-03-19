6e4266ded443bb5a6f0d15fe064d10814391a741
kenshin <kenshin@gmail.com>
2023-03-19 22:59:40 +0800

    Added the front-end function of adding books, and modified the return value of "addBook", "deleteBook" and "updateBook" methods in "BookDao" to "int".

--- src/main/java/com/kenshin/dao/BookDao.java
+++ src/main/java/com/kenshin/dao/BookDao.java
@@ -11,13 +11,13 @@ import java.util.List;
 public interface BookDao {
 
     @Insert("insert into book(id,bookName,description) values(#{id},#{bookName},#{description})")
-    public boolean addBook(Book book);
+    public int addBook(Book book);
 
     @Delete("delete from book where id = #{id}")
-    public boolean deleteBook(Integer id);
+    public int deleteBook(Integer id);
 
     @Update("update book set bookName = #{bookName} , description = #{description} where id = #{id}")
-    public boolean updateBook(Book book);
+    public int updateBook(Book book);
 
     @Select("select * from book where id = #{id}")
     public Book getBook(Integer id);
--- src/main/java/com/kenshin/service/Impl/BookServiceImpl.java
+++ src/main/java/com/kenshin/service/Impl/BookServiceImpl.java
@@ -17,17 +17,17 @@ public class BookServiceImpl implements BookService {
 
     @Override
     public boolean addBook(Book book) {
-        return bookDao.addBook(book);
+        return bookDao.addBook(book) > 0;
     }
 
     @Override
     public boolean deleteBook(Integer id) {
-        return bookDao.deleteBook(id);
+        return bookDao.deleteBook(id) > 0;
     }
 
     @Override
     public boolean updateBook(Book book) {
-        return bookDao.updateBook(book);
+        return bookDao.updateBook(book) > 0;
     }
 
     @Override
@@ -35,7 +35,7 @@ public class BookServiceImpl implements BookService {
         try {
             return bookDao.getBook(id);
         } catch (Exception e) {
-            throw new SystemException(Code.BUSINESS_ERR,"服务器访问超时",e);
+            throw new SystemException(Code.BUSINESS_ERR, "服务器访问超时", e);
         }
     }
 
