371c63ccb73dcd2cd688f47bd8a17d40925725f9
kenshin <kenshin@gmail.com>
2023-03-19 22:12:07 +0800

    Added a front-end page and read the resources of the front-end page through "ResourceHandle".

--- src/main/java/com/kenshin/config/SpringMvcConfig.java
+++ src/main/java/com/kenshin/config/SpringMvcConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
-@ComponentScan("com.kenshin.controller")
+@ComponentScan({"com.kenshin.controller","com.kenshin.config"})
 @EnableWebMvc
 public class SpringMvcConfig {
 }
